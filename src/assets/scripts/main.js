a1b0ecd40aa6008832e75575f246287ab00b84b7
Laura Corral López <laura.corral.next@bbva.com>
2023-11-03 17:56:26 +0100

    modified js file

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,4 +1,3 @@
-/* Function to open dropdown menu  */
 function navBarOnClick() {
   document.querySelector('div.navbar__dropdrown').style.display = 'block';
   document.querySelector('div.navbar__bars-close').style.display = 'block';
@@ -6,7 +5,6 @@ function navBarOnClick() {
   document.querySelector('header').style.height = '260px';
 }
 
-/* Function to close dropdown menu  */
 function navBarCloseOnClick() {
   document.querySelector('div.navbar__dropdrown').style.display = 'none';
   document.querySelector('div.navbar__bars-close').style.display = 'none';
