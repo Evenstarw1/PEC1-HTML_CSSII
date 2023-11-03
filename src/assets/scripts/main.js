d2dfcc480bce78b03a0529a1c2f8273b0f71f47d
Laura Corral López <laura.corral.next@bbva.com>
2023-11-03 19:24:51 +0100

    modified styles naming

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,13 +1,13 @@
 function navBarOnClick() {
   document.querySelector('div.navbar__dropdrown').style.display = 'block';
-  document.querySelector('div.navbar__bars-close').style.display = 'block';
-  document.querySelector('div.navbar__bars-open').style.display = 'none';
+  document.querySelector('div.navbar__bars_close').style.display = 'block';
+  document.querySelector('div.navbar__bars_open').style.display = 'none';
   document.querySelector('header').style.height = '260px';
 }
 
 function navBarCloseOnClick() {
   document.querySelector('div.navbar__dropdrown').style.display = 'none';
-  document.querySelector('div.navbar__bars-close').style.display = 'none';
-  document.querySelector('div.navbar__bars-open').style.display = 'block';
+  document.querySelector('div.navbar__bars_close').style.display = 'none';
+  document.querySelector('div.navbar__bars_open').style.display = 'block';
   document.querySelector('header').style.height = 'unset';
 }
