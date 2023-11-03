721e48076316ffecc5bc01e6d0c81248d77f9070
Laura Corral López <laura.corral.next@bbva.com>
2023-11-03 19:42:20 +0100

    modified onclick event

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,11 +3,11 @@ function navBarOnClick() {
   document.querySelector('div.navbar__bars_close').style.display = 'block';
   document.querySelector('div.navbar__bars_open').style.display = 'none';
   document.querySelector('header').style.height = '260px';
-}
+};
 
 function navBarCloseOnClick() {
   document.querySelector('div.navbar__dropdrown').style.display = 'none';
   document.querySelector('div.navbar__bars_close').style.display = 'none';
   document.querySelector('div.navbar__bars_open').style.display = 'block';
   document.querySelector('header').style.height = 'unset';
-}
+};
